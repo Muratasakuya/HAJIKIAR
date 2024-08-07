1776a28f8a58cb3bc256af7fe246738479b14708
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2024-08-07 14:13:08 +0900

    変更なし

--- Objects/GameObject3D/GameObject3D.h
+++ Objects/GameObject3D/GameObject3D.h
@@ -1,6 +1,14 @@
 #pragma once
 
+// Engine
+#include "Engine.h"
 
+// 構造体
+#include "CBufferStructure.h"
+
+// c++
+#include <string>
+#include <filesystem>
 
 /*////////////////////////////////////////////////////////////////////////////////
 *								GameObject3D Class
@@ -11,9 +19,36 @@ public:
 	///			メンバ関数
 	/*-----------------------------*/
 
+	GameObject3D();
+	~GameObject3D();
+
+	void Initialize();
+	void Update();
+	void Draw();
+
 private:
 	/*-----------------------------*/
 	///			メンバ変数
 	/*-----------------------------*/
+
+	/*--------------------------------------------------------------*/
+	// メインオブジェクト
+
+	// SRT
+	Transform transform_;
+
+	// マテリアル
+	Material material_;
+
+	// ライト
+	PunctualLight light_;
+
+	/*--------------------------------------------------------------*/
+	// サブオブジェクト
+
+	// 使用するテクスチャの名前
+	std::string textureName_;
+	// 使用するモデルの名前
+	std::string modelName_;
 
 };
